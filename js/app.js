bb237a2d9d3289fbdb2142bccad530de7310b039
pierdr <pierdr@yahoo.it>
2013-06-06 17:19:29 +0200

    slider almost done

--- js/app.js
+++ js/app.js
@@ -7,15 +7,29 @@ $(function() {
 	var list = overview.find('.list');
 	var search = overview.find('.search-bar');
 	var detailBack = detail.find('.back');
+
 	var timeline_labels = $("#timeline .label");
-	var timeline_actuator_days = ["TODAY","TOMORROW","FRIDAY"];
-	var timeline_actuator_times = ["03:00","06:00","09:00","12:00","15:00","18:00","21:00","24:00"]
+	var timeline_actuator_days = ["TODAY", "TOMORROW", "FRIDAY"];
+	var timeline_actuator_times = ["03:00", "06:00", "09:00", "12:00", "15:00", "18:00", "21:00", "24:00"]
 	var timeline_actuator = $("#timeline-actuator");
-	timeline_actuator.hW=Math.round(parseInt(timeline_actuator.css("width"))/2);
-	timeline_actuator.W=parseInt(timeline_actuator.css("width"));
+	var timeline_line = $("#timeline-line");
+	var timeline_actuator_label_day=$("#timeline-actuator .day");
+	var timeline_actuator_label_time=$("#timeline-actuator .time");
+	timeline_actuator.hW = Math.round(parseInt(timeline_actuator.css("width")) / 2);
+	timeline_actuator.W = parseInt(timeline_actuator.css("width"));
+	window.static = {};
+	window.previousX = 0;
+	window.current = {};
+
+	var svg = $("svg");
+	var rects = [];
+	$("svg").each(function(i) {
+		rects[i] = $(this).find("rect");
+	});
 	
-	var numberOfSlices=24;
-	
+
+	var numberOfSlices = window.static.numOfSlices = 24;
+
 	var loadList = function() {
 		list.hide();
 		loader.show();
@@ -93,45 +107,120 @@ $(function() {
 	}
 	initLayout();
 
-	var setTimeSelected = function(index,day,time) {
-		//base 24 --> total num of bars
-		var tmpIndex = index;
-		$("svg").find("rect").attr("class", "");
-		$("svg").each(function() {
-			$(this).find("rect").each(function(i) {
-				
-				if (i == tmpIndex) {
-					$(this).attr("class", "selected");
-				}
+	var setTimeSelected = function(index, day, time) {
+		if (day < timeline_actuator_days.length) {
+			//base 24 --> total num of bars
+			var tmpIndex = index;
+			$("svg").find("rect").attr("class", "");
+			$("svg").each(function() {
+				$(this).find("rect").each(function(i) {
+
+					if (i == tmpIndex) {
+						$(this).attr("class", "selected");
+					}
+				});
 			});
-		});
-		//base 3 --> num of days
-		$("#timeline-actuator .day").html(timeline_actuator_days[day]);
-		//base 8 --> num of time slices/day
-		$("#timeline-actuator .time").html(timeline_actuator_times[time]);
-		
+			//base 3 --> num of days
+			timeline_actuator_label_day.html(timeline_actuator_days[day]);
+			//base 8 --> num of time slices/day
+			timeline_actuator_label_time.html(timeline_actuator_times[time]);
+		}
 	}
-	setTimeSelected(3,0,3);
+	var moveSliderTo = function(posX) {
+		var compensate = window.current.day * 5 + 3;
+		posX += compensate;
+		
+		if (posX > 0 && posX < $(window).width() - timeline_actuator.W) {
+			timeline_actuator.css({
+				"-webkit-transform" : "translateX(" + posX + "px)"
+			});
+
+			posX += timeline_actuator.hW - 10;
+			timeline_line.css({
+				"-webkit-transform" : "translateX(" + posX + "px)"
+			});
+			$("#timeline-actuator .arrow").css("-webkit-transform", "translateX(" + 0 + "px)");
+
+		} else if (posX <= 0) {
+
+			posX += timeline_actuator.hW - 10;
+			timeline_line.css({
+				"-webkit-transform" : "translateX(" + posX + "px)"
+			});
+
+			posX -= -12 + timeline_actuator.hW;
+			$("#timeline-actuator .arrow").css("-webkit-transform", "translateX(" + posX + "px)");
+
+			posX = 0;
+			timeline_actuator.css({
+				"-webkit-transform" : "translateX(" + posX + "px)"
+			});
+
+		} else if (posX >= $(window).width() - timeline_actuator.W) {
+			posX += timeline_actuator.hW - 10;
+
+			if (posX > $(window).width() - 14) {
+				posX = $(window).width() - 14;
+			}
+			timeline_line.css({
+				"-webkit-transform" : "translateX(" + posX + "px)"
+			});
+
+			posX = (timeline_actuator.hW + 12) - ($(window).width() - posX);
+
+			$("#timeline-actuator .arrow").css("-webkit-transform", "translateX(" + posX + "px)");
+
+			posX = $(window).width() - timeline_actuator.W;
+			timeline_actuator.css({
+				"-webkit-transform" : "translateX(" + posX + "px)"
+			});
+
+		}
+	}
+	setTimeSelected(2, 0, 2);
+	moveSliderTo(0);
 
 	timeline_actuator.bind("touchmove", function(evt) {
-
+		evt.preventDefault();
+		var posX = evt.originalEvent.touches[0].pageX;
 		if (evt.originalEvent.touches[0].pageX < $(window).width() && evt.originalEvent.touches[0].pageX > 0) {
-			var posX= evt.originalEvent.touches[0].pageX;
-			var actTmpPos=(posX<timeline_actuator.hW)? 0 : (posX>$(window).width()-timeline_actuator.hW)?$(window).width()-timeline_actuator.W:posX-timeline_actuator.hW;
-			
-			
-			$("#timeline-actuator").css({"margin-left":actTmpPos}).addClass("moved");
-			var currentSliceNum=Math.round(posX/(Math.round($(window).width()/numberOfSlices)));
-			var currentDayIndex=Math.floor(currentSliceNum/(timeline_actuator_times.length));
-			var currentTimeIndex=currentSliceNum%timeline_actuator_times.length;
-			if(currentSliceNum>=numberOfSlices)
-			{
-				currentSliceNum=numberOfSlices-1;
+			if (window.previousX != 0) {
+
+				//var actTmpPos = (posX < timeline_actuator.hW) ? 0 : (posX > $(window).width() - timeline_actuator.hW) ? $(window).width() - timeline_actuator.W : posX - timeline_actuator.hW;
+				var actTmpPos = window.current.posX - (window.previousX - posX);
+
+				var currentSliceNum = Math.round(posX / (Math.round($(window).width() / numberOfSlices)));
+				var currentDayIndex = Math.floor(currentSliceNum / (timeline_actuator_times.length));
+				var currentTimeIndex = currentSliceNum % timeline_actuator_times.length;
+
+				/*if (currentSliceNum >= numberOfSlices) {
+				 currentSliceNum = numberOfSlices - 1;
+
+				 }*/
+				setTimeSelected(currentSliceNum, currentDayIndex, currentTimeIndex);
+				window.current.posX = actTmpPos;
+				window.current.slice = currentSliceNum;
+				window.current.day = currentDayIndex;
+				window.current.time = currentTimeIndex;
+				moveSliderTo(actTmpPos);
 			}
-			
-			console.log(currentDayIndex,currentTimeIndex);
-			setTimeSelected(currentSliceNum,currentDayIndex,currentTimeIndex);
-			
+
+			window.previousX = posX;
+
 		}
+
+	}).bind("touchstart", function() {
+		$(this).addClass("pressed");
+		var tmp = timeline_actuator.css('-webkit-transform');
+		tmp = tmp.match(/[0-9\.]+/g);
+		if (tmp != null) {
+			window.current.posX = parseInt(tmp[4]);
+		}
+	}).bind("touchend", function() {
+
+		window.previousX = 0;
+		/*var posXtmp = window.current.slice * Math.round($(window).width() / window.static.numOfSlices) - timeline_actuator.hW + compensate;
+		moveSliderTo(posXtmp);
+		$(this).removeClass("pressed");*/
 	});
 });
